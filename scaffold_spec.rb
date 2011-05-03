3f91b6d5e751cc1bade4325f2ef17947990427d0
Thomas Newton <thomas@newtonlabs.org>
2011-05-03 08:34:06 -0400

    first commit

---
+++ scaffold_spec.rb
@@ -0,0 +1,67 @@
+Scaffoldhub::Specification.new do
+
+  # Github URL where you will post your scaffold - the specified folder must contain this file
+  base_url  'https://github.com/newtonlabs/graphing_scaffold'
+
+  # The name of your new scaffold: should be a single word
+  name 'graphing'
+
+  # Metadata about this scaffold - these values are only used for display on scaffoldhub.org:
+  metadata do
+
+    # A short paragraph describing what this scaffold does
+    description 'The graphing scaffold.'
+
+    # 4x3 aspect ratio screen shot
+    screenshot 'graphing_screenshot.png'
+
+    # Tag(s) to help scaffoldhub.org users find your scaffold
+    tag 'jquery'
+    tag 'graph'
+    tag 'graphing'
+
+    # Optionally specify an example of a scaffold parameter
+    parameter_example 'FIELD_NAME'
+
+    # Optionally post a link to an article you write explaining how the scaffold works.
+    blog_post 'http://patshaughnessy.net/2011/3/13/view-mapper-for-rails-3-scaffoldhub'
+  end
+
+  # Define a model template - this ERB file will be used to generate a new
+  # model class with this path & filename: app/models/NAME.rb
+  model 'templates/model.rb'
+
+  # Define an ActiveRecord migration template - this ERB file will be used to generate a new
+  # migration class with this path & filename: db/migrate/TIMESTAMP_create_PLURAL_NAME.rb
+  migration 'templates/migration.rb'
+
+  # Define a controller template - this ERB file will be used to generate a new
+  # controller class with this path & filename: app/controllers/PLURAL_NAME.rb
+  controller 'templates/controller.rb'
+
+  # You can use "with_options" to specify the same source folder for a series of templates:
+  # You can also specify the same destination folder using the :dest option,
+  # or just use the :src and :dest options on each keyword.
+  with_options :src => 'templates' do
+    view '_form.html.erb'
+    view 'new.html.erb'
+    view 'edit.html.erb'
+    view 'index.html.erb'
+    view 'show.html.erb'
+  end
+
+  # Other keywords available are:
+
+  # Define any other generic template - this ERB file will be used to generate
+  # a new file - the dest option is required to indicate where to put the new file
+  # template 'xyz.html.erb', :dest => 'path/to/xyz.html'
+  template 'templates/graphs.js',   :dest => 'public/javascripts'
+
+  # Copy any file without running an ERB transformation
+  # file 'xyz.html', :dest => 'path/to/xyz.html'
+  
+
+  file 'templates/highcharts.js',     :dest => 'public/javascripts'
+  file 'templates/excanvas.min.js',   :dest => 'public/javascripts'
+
+end
---
+++ templates/graphs.js
@@ -0,0 +1,45 @@
+var chart;
+
+// Utility Function for loading Charts over JS
+(function($){ 
+  $.loadChart = function (options, url) {
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        for( i=0; i < data.length; i++){
+          options.series[i].data = data[i]
+        }
+        new Highcharts.Chart(options);
+      },
+      error: function(status, statusText, responses, headers) {
+        alert('Oops something went wrong..');
+      }
+    });
+  }
+})(jQuery);
+
+
+$(document).ready(function() {
+  // JSON Object that describes how the Chart should look and where to render too
+  chart = {
+    chart: {
+      renderTo: '<%= plural_table_name %>'
+    },
+    title: {
+      text: 'Graphing <%= plural_table_name %>'
+    },
+    tooltip: {
+      formatter: function() {
+        return '<b>'+ this.point.name +'</b>: '+ this.y;
+      }
+    },
+    series: [{
+      type: 'pie'
+    }]
+  };
+  
+  // Load the Chart over AJAX, Need a better way to find this controller
+  $.loadChart(chart, '/<%=plural_table_name%>.json');
+});
